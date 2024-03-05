d6737447624ae682f79ff388ead1ca28bb9bf556
LightChimera <artemilin347@gmail.com>
2024-03-05 16:52:35 +0300

    Я чайник!

---
+++ TaskManager/TaskManager.API/Controllers/TeapotController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TaskManager.API.Controllers
+{
+    [Route("api/v1"), ApiController]
+    public class TeapotController : Controller
+    {
+        [Authorize]
+        [HttpGet("/makecoffee/")]
+        public IResult Index(string coffeeType)
+        {
+            if (coffeeType.IsNullOrEmpty())
+                return Results.BadRequest("Can`t be null");
+
+            HttpContext.Response.StatusCode = 418;
+            return Results.BadRequest("I`m a teapot!");
+        }
+    }
+}
