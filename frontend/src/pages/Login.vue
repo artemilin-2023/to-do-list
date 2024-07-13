490a10180d6acd6b4f54a4ae60c8019139ed38fa
LightChimera <artemilin347@gmail.com>
2024-07-13 10:59:28 +0300

    Доделать валидацию форм на странице авторизации

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,35 +1,43 @@
 <template>
   <q-page padding>
-    <div class="flex flex-center">
-      <q-input
-        class="q-ma-sm"
-        style="width: 100%"
-        v-model="email"
-        type="email"
-        label="Почта"
-      />
-      <password-input
-        ref="passwordInput"
-        class="q-ma-sm"
-        style="width: 100%"
-        :label="'Пароль'"
-      />
-      <q-btn
-        style="width: 100%"
-        class="q-ma-sm"
-        color="primary"
-        label="Войти"
-        @click="login()"
-      />
+    <div>
+      <q-form class="flex flex-center" @submit="login()">
+        <q-input
+          class="q-ma-sm"
+          style="width: 100%"
+          v-model="email"
+          type="email"
+          label="Почта"
+          :rules="[
+            $rules.required('Почта обязательна для заполнения.'),
+            $rules.email('Пожалуйста, введите настоящую почту.'),
+          ]"
+        />
+        <password-input
+          ref="passwordInput"
+          class="q-ma-sm"
+          style="width: 100%"
+          :label="'Пароль'"
+          :rules="[$rules.required('Пароль обязателен для заполнения.')]"
+        />
+        <q-btn
+          style="width: 100%"
+          type="submit"
+          class="q-ma-sm"
+          color="primary"
+          label="Войти"
+          @click="login()"
+        />
 
-      <q-btn
-        class="q-mt-sm"
-        size="sm"
-        flat
-        color="secondary"
-        label="Не помню пароль"
-        to="/auth/resetpassword"
-      />
+        <q-btn
+          class="q-mt-sm"
+          size="sm"
+          flat
+          color="secondary"
+          label="Не помню пароль"
+          to="/auth/resetpassword"
+        />
+      </q-form>
     </div>
   </q-page>
 </template>
--- frontend/src/pages/Register.vue
+++ frontend/src/pages/Register.vue
@@ -1,61 +1,82 @@
 <template>
   <q-page padding>
-    <div class="flex flex-center">
-      <q-input
-        class="q-ma-sm"
-        style="width: 100%"
-        v-model="email"
-        type="email"
-        label="Почта"
-        :error="v$.$dirty"
-      />
-      <q-input
-        class="q-ma-sm"
-        style="width: 100%"
-        v-model="nickname"
-        type="nickname"
-        label="Никнейм"
-      />
-      <password-input
-        ref="password"
-        class="q-ma-sm"
-        style="width: 100%"
-        :label="'Пароль'"
-      />
-      <password-input
-        ref="confirmPassword"
-        class="q-ma-sm"
-        style="width: 100%"
-        :label="'Повторите пароль'"
-      />
-      <q-btn
-        :disable="v$.$invalid"
-        type="submit"
-        style="width: 100%"
-        class="q-ma-sm"
-        color="primary"
-        label="Зарегистрироваться"
-        @click="register()"
-      />
+    <div>
+      <q-form
+        ref="form"
+        class="flex flex-center"
+        @submit="tryRegister()"
+        @reset="reset()"
+      >
+        <q-input
+          style="width: 100%"
+          v-model="email"
+          type="email"
+          label="Почта"
+          lazy-rules
+          :rules="[
+            $rules.required('Почта обязательна для заполнения.'),
+            $rules.email('Пожалуйста, введите настоящую почту.'),
+          ]"
+        />
+        <q-input
+          style="width: 100%"
+          v-model="nickname"
+          type="nickname"
+          label="Никнейм"
+          :rules="[$rules.required('Никнейм обязателен для заполнения.')]"
+        />
+        <password-input
+          ref="password"
+          style="width: 100%"
+          :label="'Пароль'"
+          :rules="[
+            $rules.required('Пароль обязателен для заполнения.'),
+            $rules.minLength(6, 'Пароль слишком короткий.'),
+          ]"
+        />
+        <password-input
+          ref="confirmPassword"
+          style="width: 100%"
+          :label="'Повторите пароль'"
+          :rules="[
+            $rules.required('Пароль обязателен для заполнения.'),
+            $rules.sameAs(
+              this.$refs.password ? this.$refs.password.password : '',
+              'Пароли не совпадают.'
+            ),
+          ]"
+        />
+
+        <div class="flex flex-center">
+          <q-btn
+            type="submit"
+            style="width: 100%"
+            class="q-ma-sm"
+            color="primary"
+            label="Зарегистрироваться"
+          />
+          <q-btn
+            label="Сбросить"
+            type="reset"
+            color="secondary"
+            style="width: 100%"
+            flat
+            class="q-ml-sm"
+          />
+        </div>
+      </q-form>
     </div>
   </q-page>
 </template>
 
 <script>
+import { email } from "@vuelidate/validators";
 import PasswordInput from "src/components/PasswordInput.vue";
-import { required } from "@vuelidate/validators";
 import { ref } from "vue";
-import useVuelidate from "@vuelidate/core";
 
 export default {
   name: "RegisterPage",
   components: { PasswordInput },
-
-  setup() {
-    return {
-      v$: useVuelidate(),
-    };
-  },
 
   data() {
     return {
@@ -65,20 +86,25 @@ export default {
   },
 
   methods: {
-    register() {},
-  },
+    reset() {
+      this.$refs.form.resetValidation();
+      this.email = ref("");
+      this.nickname = ref("");
+      this.$refs.password.password = ref("");
+      this.$refs.confirmPassword.password = ref("");
+    },
 
-  validations() {
-    return {
-      email: {
-        required,
-        validFormat: (val) => /^\S+@\S+\.\S+$/,
-        $autoDirty: true,
-      },
-      nickname: {
-        required,
-      },
-    };
+    tryRegister() {
+      this.$refs.form.validate().then((isSuccess) => {
+        if (isSuccess) {
+          this.register();
+        }
+      });
+    },
+
+    register() {
+      console.log("reg");
+    },
   },
 };
 </script>
